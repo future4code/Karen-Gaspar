502a0555fa7514179f69c31cf4ab584930152f77
KarenGaspar <karenasgaspar@gmail.com>
2021-10-08 18:25:42 -0300

    Projeto da segunda semana

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,136 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  // implemente sua lógica aqui
+  const altura = Number(prompt("Digite a altura do retangulo"))
+  const largura = Number(prompt("Digite a largura do retangulo"))
+  const area = altura * largura
+  console.log(area)
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  // implemente sua lógica aqui
+  anoAtual = Number(prompt("Digite o ano atual"))
+  anoNascimento = Number(prompt("Digite o ano de seu nascimento"))
+  console.log(anoAtual - anoNascimento)
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+  const calculo1 = (altura * altura)
+  const calculo2 = peso / calculo1
+  return calculo2
+}
+const imc = calculo2
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  nome = prompt("Digite o seu nome")
+  idade = prompt("Digite dua idade")
+  email = prompt("Digite seu e-mail")
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+  cor [0] = prompt("Digite sua primeira cor favorita")
+  cor [1] = prompt("Digite sua segunda cor favorita")
+  cor [2] = prompt("Digite sua terceira cor favorita")
+  
+  console.log(cor[0,1,2])
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+  return string.toUpperCase()
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+return custo / valorIngresso
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+return string1.length === string2.length
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+return array[0]
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+return array[array.length -1]
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+  const numeroInicial = array[0]
+  const numeroFinal = [array.length -1]
+  return numeroInicial.splice(0,0, numeroFinal)
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+return string1.toUpperCase() === string2.toUpperCase()
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+anoCorrente = prompt ("Digite o ano atual")
+anoDeNascim = prompt ("Digite o ano de seu nascimento")
+anoRg = prompt ("Digite o ano de emissão do seu RG")
+
+const renovaEm5 = anoCorrente - anoDeNascim <= 20
+const renovaEm10 = anoCorrente - anoNascimento > 20 <= 50
+const renovaEm15 = anoCorrente - anoNascimento > 50
+console.log()
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+idade = prompt("Você tem mais de 18 anos?")
+escolaridade = prompt("Você possui Ensino Médio Completo?")
+disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+console.log()
+}
