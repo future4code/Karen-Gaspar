a79885a1e0a2bb0b043dcbcdd34e6780d18b9872
KarenGaspar <karenasgaspar@gmail.com>
2021-10-18 18:41:32 -0300

    Exercício 5

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -31,12 +31,16 @@ function retornaNumerosPares(array) {
 
 
 // EXERCÍCIO 05
-// function retornaNumerosParesElevadosADois(array) {
-//     const numerosPares = array.filter((numero) => {
-//         return numero % 2 === 0
-//     })
-//     return numerosPares
-// }
+function retornaNumerosParesElevadosADois(array) {
+    const numPares = array.filter((num) => {
+        return num % 2 === 0
+    })
+    const numerosElevados = numPares.map((elevados) => {
+        return elevados * elevados
+    })
+    return numerosElevados
+}
+retornaNumerosParesElevadosADois()
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
@@ -49,70 +53,94 @@ function retornaMaiorNumero(array) {
     return maior
 }
 
-
 // EXERCÍCIO 07
-// function retornaObjetoEntreDoisNumeros(num1, num2) {
-//     const comparacao = {
-//         maiorNumero: ,
-//         maiorDivisivelPorMenor:,
-//         diferenca: 
-//         }
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+    const valor1 = 0
+    const valor2 = 0
+    
+    let numero = {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: maior % menor === 0,
+        diferenca: maior - menor
+    }
 
-//     return
-// }
+    return numero
+}
 
-// EXERCÍCIO 08
+// // EXERCÍCIO 08
 // function retornaNPrimeirosPares(n) {
 //     let pares = array[0]
-//     for (let i = 0; i < array.length; i+=2) {
+//     for (let i = 0; i < array.length; i += 2) {
 //         if (array[i] > pares) {
 //             pares = array[i]
 //         }
 //     }
-//     return n
+//     return
 // }
 
-// EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {
+// // EXERCÍCIO 09
+// function classificaTriangulo(ladoA, ladoB, ladoC) {
 
-}
+//     if (ladoA === ladoB && ladoB === ladoC && ladoA === ladoC) {
+//         return "Equilátero"
+//     } else if (ladoA === ladoB && ladoB !== ladoC && ladoA !== ladoC) {
+//         return "Isósceles"
+//     } else if (ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {
+//         return "Escaleno"
+//     } else {
 
-// EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {
+//     }
+//     return classificaTriangulo()
+// }
 
-}
+// // EXERCÍCIO 10
+// function retornaSegundoMaiorESegundoMenor(array) {
+//     const arrayOrdenado = array.sort(function (a, b) {
+//         const segundoMaiorValor = arrayOrdenado[arrayOrdenado.lenght - 2]
+//         const segundoMenorValor = arrayOrdenado[1]
+//         arrayFinal = [segundoMaiorValor, segundoMenorValor]
+//     })
+//     return arrayFinal
+// }
 
-// EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
+// // EXERCÍCIO 11
+// function retornaChamadaDeFilme(filme) {
+//     let filme = {
+//         nome: "",
+//         ano: Number,
+//         diretor: "",
+//         atores: [],
+//     }
+//     return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores}`
+// }
+// retornaChamadaDeFilme()
 
-}
+// // EXERCÍCIO 12
+// function retornaPessoaAnonimizada(pessoa) {
 
-// EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {
+// }
 
-}
+// // EXERCÍCIO 13A
+// function retornaPessoasAutorizadas(pessoas) {
 
-// EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {
+// }
 
-}
+// // EXERCÍCIO 13B
+// function retornaPessoasNaoAutorizadas(pessoas) {
 
-// EXERCÍCIO 13B
-function retornaPessoasNaoAutorizadas(pessoas) {
+// }
 
-}
+// // EXERCÍCIO 14
+// function retornaContasComSaldoAtualizado(contas) {
 
-// EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
+// }
 
-}
+// // EXERCÍCIO 15A
+// function retornaArrayOrdenadoAlfabeticamente(consultas) {
 
-// EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {
+// }
 
-}
+// // EXERCÍCIO 15B
+// function retornaArrayOrdenadoPorData(consultas) {
 
-// EXERCÍCIO 15B
-function retornaArrayOrdenadoPorData(consultas) {
-
-}
+// }
