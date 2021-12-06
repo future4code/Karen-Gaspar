02d3c050890707b8289db901f8d1285ef7d8c5b4
Karen Gaspar <karenasgaspar@gmail.com>
2021-12-06 17:01:09 -0300

    Deletei o import de fonte externa do Google

--- modulo3/projeto-astromatch/src/GlobalStyle.js
+++ modulo3/projeto-astromatch/src/GlobalStyle.js
@@ -2,8 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Lilita+One&family=Lobster&display=swap');
-
 *{
     margin: 0;
     padding: 0;
