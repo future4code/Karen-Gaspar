d7b955fa012a351c72aa8b1c90e1bb413b717e43
Karen Gaspar <karenasgaspar@gmail.com>
2022-01-26 12:32:14 -0300

    Fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,19 @@
+type Product = {id: string, name: string, price: number}
+
+export const products: Product[] = [
+    {
+    id: "k1",
+    name: "Blusa",
+    price: 30
+    },
+    {
+    id: "k2",
+    name: "Calça",
+    price: 70
+    },
+    {
+    id: "k3",
+    name: "Sapato",
+    price: 50
+    },
+]
