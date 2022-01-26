33ae84a3bff112e9b9cba79c2d2654c6b20f42be
Karen Gaspar <karenasgaspar@gmail.com>
2022-01-26 17:20:23 -0300

    Exercícios 5,6,7,8 e 9 feitos

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import {Product, products} from "./data"
+import { Product, products } from "./data"
 
 import { AddressInfo } from "net";
 
@@ -8,12 +8,12 @@ const app = express();
 app.use(express.json());
 
 const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
-  } else {
-    console.error(`Failure upon starting server.`);
-  }
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
 });
 
 // Exercício 1
@@ -23,28 +23,58 @@ const server = app.listen(process.env.PORT || 3003, () => {
 // })
 
 
-//Exercício 3
+//Exercício 3 e 7
 
 app.post("/products", (req: Request, res: Response) => {
-    try{
-        const {name, price} = req.body
+    try {
+        const { name, price } = req.body
+
+        if (price <= 0) {
+            throw new Error("Preço deve ser maior que zero")
+        }
 
         if (!name || !price) {
-            throw new Error ("Campos necessários para criar produto não foram informados")
+            throw new Error("Campos necessários para criar produto não foram informados")
         }
+
+        if (typeof name !== "string") {
+            throw new Error("Nome do produto deve ser do tipo string")
+        }
+
+        if (typeof price !== "number") {
+            throw new Error("Preço do produto deve ser do tipo number")
+        }
+
         const newProduct: Product = {
             id: Date.now().toString(),
             name,
             price
         }
         products.push(newProduct)
-    
+
         res.status(200).send(products)
     }
 
     catch (error: any) {
-    res.status(400).send(error.message)
+        switch (error.message) {
+            case "Campos necessários para criar produto não foram informados":
+                res.status(422)
+                break
+            case "Nome do produto deve ser uma string":
+                res.status(422)
+                break
+            case "Preço do produto deve ser um number":
+                res.status(422)
+                break
+            case "Preço deve ser maior que zero":
+                res.status(422)
+                break
+            default:
+                res.status(500)
+        }
+        res.send(error.message)
     }
+
 })
 
 
@@ -54,30 +84,107 @@ app.get("/products", (req: Request, res: Response) => {
     res.status(200).send(products)
 })
 
-// Exercício 5
+// Exercício 5 e 8
 
-// app.put("/products/:id/price", (req: Request, res: Response) => {
-    
-//     try{
+app.put("/products/:id/price", (req: Request, res: Response) => {
 
-//     const productId = req.params.id
+    try {
 
-//     if (!productId) {
-//         throw new Error ("Id do produto não informado")
-//     }
+        const id = req.params.id
+        const newPrice = req.body.price
 
-//     const newPrice = {}
 
-//     for (let product of products) {
-//         if (product.id === productId) {
+        if (!id) {
+            throw new Error("ID do produto não informado")
+        }
 
-//         }
-//     }
-    
-//     res.status(200).send(products)
-//     }
+        if (!newPrice) {
+            throw new Error("Preço do produto não informado")
+        }
 
-//     catch (error: any) {
-//         res.status(400).send(error.message)
-//     }
-// })
+        if (typeof newPrice !== "number" || newPrice <= 0) {
+            throw new Error("Preço do produto deve ser do tipo number maior que zero")
+        }
+
+        let isProductFound = false
+
+        for (let product of products) {
+            if (product.id === id) {
+                product.price = newPrice
+                isProductFound = true
+            }
+        }
+
+        if (!isProductFound) {
+            throw new Error("Produto não encontrado")
+        }
+
+        res.status(200).send(products)
+    }
+
+    catch (error: any) {
+        switch (error.message) {
+            case "Id do produto não informado":
+                res.status(422)
+                break
+            case "Preço do produto não informado":
+                res.status(422)
+                break
+            case "Preço do produto deve ser do tipo number":
+                res.status(422)
+                break
+            case "Preço deve ser maior que zero":
+                res.status(422)
+                break
+            case "Produto não encontrado":
+                res.status(404)
+                break
+            default:
+                res.status(500)
+        }
+        res.send(error.message)
+    }
+})
+
+// Exercício 6 e 9
+
+app.delete("/products/:id", (req: Request, res: Response) => {
+
+    try {
+        const id = req.params.id
+
+        if (!id) {
+            throw new Error("Id do produto não informado")
+        }
+
+        let isProductFound = false
+
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
+                products.splice(i, 1)
+                isProductFound = true
+            }
+        }
+
+        if (!isProductFound) {
+            throw new Error("Produto não encontrado")
+        }
+
+        res.status(200).send(products)
+    }
+
+    catch (error: any) {
+        switch (error.message) {
+            case "Id do produto não informado":
+                res.status(422)
+                break
+            case "Produto não encontrado":
+                res.status(404)
+                break
+            default:
+                res.status(500)
+        }
+        res.send(error.message)
+    }
+})
+
