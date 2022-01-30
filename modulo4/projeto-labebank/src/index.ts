91b7b884f7acff258c40f26fd352568c811343ab
Karen Gaspar <karenasgaspar@gmail.com>
2022-01-29 22:20:19 -0300

    Função de pegar o client por cpf

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -30,27 +30,33 @@ app.post("/clients", (req: Request, res: Response) => {
     
     const { name, cpf, birthDate} = req.body
 
-    // o type string[] transforma a data em array e junto com o split elimina a / entre os números
+    const sameCpf = clients.find((client)=> client.cpf === req.body.cpf)
+   
     const clientBirthDate: string[] = birthDate.split("/")
-    // novo objeto substitui o type da data de nascimento
+   
     const birthDateConvert: Date ={
       day: Number(clientBirthDate[0]),
       month: Number(clientBirthDate[1]),
       year: Number(clientBirthDate[2]),
     }
-    // variável para definir a data atual com mesmo type da data de nascimento convertida
+    
     const currentDate: Date = {
       day: new Date().getDate(),
       month: new Date().getMonth() + 1,
       year: new Date().getFullYear(),
     }
 
-    // condicional de preenchimento de dados obrigatórios
+    
     if (!name || !cpf || !birthDate) {
       errorCode = 422
       throw new Error("Please check the fields")
     }
-    // condicional de idade mínima obrigatória
+   
+    if (sameCpf){
+      errorCode = 422
+      throw new Error("This CPF is already registered")
+    }
+    
     if (currentDate.year - birthDateConvert.year < 18){
       errorCode = 401
       throw new Error("The user needs to be 18 to open an account")
@@ -81,15 +87,27 @@ app.post("/clients", (req: Request, res: Response) => {
   }
 })
 
-// app.get("/clients/:cpf", (req: Request, res: Response)=>{
-//   let errorCode = 400
-//   try{
-//     const {name, cpf} = req.params
-//     res.status(200).send("Deu bom!")
-//   }catch(error: any){
-//     res.status(errorCode).send("Deu ruim!")
-//   }
-// })
+app.get("/clients/balance", (req: Request, res: Response)=>{
+  let errorCode = 400
+  try{
+    const cpf: string = req.query.cpf as string
+    const client: Client | undefined = clients.find((client) => client.cpf === cpf)
+
+    if (!cpf){
+      errorCode = 422
+      throw new Error("Please check the queries")
+    }
+
+    if(!client){
+      errorCode = 404
+      throw new Error("Client not found")
+    }
+
+    res.status(200).send(client)
+  }catch(error: any){
+    res.status(errorCode).send({message: error.message})
+  }
+})
 
 // app.put("/clients/", (req: Request, res: Response)=>{
 //   let errorCode = 400
