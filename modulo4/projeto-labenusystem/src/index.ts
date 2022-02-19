616be5fd0c67f077b051f3fcfe9f5bba8f428124
Karen Gaspar <karenasgaspar@gmail.com>
2022-02-18 21:34:37 -0300

    Inclusão de arquivos

--- modulo4/projeto-labenusystem/src/index.ts
+++ modulo4/projeto-labenusystem/src/index.ts
@@ -1,1 +1,11 @@
 import { app } from "./app";
+import { createClass } from "./endpoints/createClass";
+import { createStudent } from "./endpoints/createStudent";
+import { createTeacher } from "./endpoints/createTeacher";
+
+
+app.post('/class', createClass)
+
+app.post('/student', createStudent)
+
+app.post('/teacher', createTeacher)
