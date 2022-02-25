492a7f66cb174e86434c3a6967886eda4f0c12f8
Karen Gaspar <karenasgaspar@gmail.com>
2022-02-24 22:56:48 -0300

    Criação de endpoint de buscar perfil do usuário

--- modulo4/projeto-cookenu/src/data/UserDatabase.ts
+++ modulo4/projeto-cookenu/src/data/UserDatabase.ts
@@ -1,30 +1,42 @@
 import { User } from "../entities/User";
 import { BaseDatabase } from "./BaseDatabase";
 
-export class UserDatabase extends BaseDatabase{
-    public async createUser(user: User){
+export class UserDatabase extends BaseDatabase {
+    public async createUser(user: User) {
         try {
             await BaseDatabase
-            .connection('cookenu_users')
-            .insert({
-                id: user.getId(),
-                name: user.getName(),
-                email: user.getEmail(),
-                password: user.getPassword()
-            })
+                .connection('cookenu_users')
+                .insert({
+                    id: user.getId(),
+                    name: user.getName(),
+                    email: user.getEmail(),
+                    password: user.getPassword()
+                })
         } catch (error: any) {
             throw new Error(error.sqlMessage || error.message)
         }
     }
-    
+
     public async findUserByEmail(email: string): Promise<User> {
         try {
             const user = await BaseDatabase
-            .connection('cookenu_users')
-            .select('*')
-            .where({email})
+                .connection('cookenu_users')
+                .select('*')
+                .where({ email })
             return user[0] && User.toUserModel(user[0])
-        } catch (error:any) {
+        } catch (error: any) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+
+    public async getUserById(id: string): Promise<User> {
+        try {
+            const result = await BaseDatabase
+                .connection('cookenu_users')
+                .select('id', 'email')
+                .where({ id })
+            return result[0]
+        } catch (error: any) {
             throw new Error(error.sqlMessage || error.message)
         }
     }
--- modulo4/projeto-cookenu/src/index.ts
+++ modulo4/projeto-cookenu/src/index.ts
@@ -1,6 +1,8 @@
 import {app} from "./app"
+import { getUserProfile } from "./endpoints/getUserProfile"
 import { login } from "./endpoints/login"
 import { signup } from "./endpoints/signup"
 
 app.post("/signup", signup)
 app.post("/login", login)
+app.get("/profile", getUserProfile)
