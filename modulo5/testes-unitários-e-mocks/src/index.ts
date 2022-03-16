11fea91ed6496f99977fc4361ada14f334c34b18
Karen Gaspar <karenasgaspar@gmail.com>
2022-03-15 21:34:40 -0300

    Exercício 3

--- modulo5/testes-unitários-e-mocks/src/index.ts
+++ modulo5/testes-unitários-e-mocks/src/index.ts
@@ -21,3 +21,28 @@ export const validateCharacter = (input: Character): boolean => {
 
     return true;
 };
+
+
+// export const performAttack = (attacker: Character, defender: Character) => {
+//     if (!validateCharacter(attacker) || !validateCharacter(defender)) {
+//         throw new Error("Invalid character");
+//     }
+
+//     if (attacker.strength > defender.defense) {
+//         defender.life -= attacker.strength - defender.defense;
+//     }
+// };
+
+export const performAttack = (
+    attacker: Character,
+    defender: Character,
+    validator: (input: Character) => boolean
+) => {
+    if (!validator(attacker) || !validator(defender)) {
+        throw new Error("Invalid character");
+    }
+
+    if (attacker.strength > defender.defense) {
+        defender.life -= attacker.strength - defender.defense;
+    }
+};
